da8cae63e7e431a7e63dcfcd70d809e66c381528
Aleksandar <arekusanda1@gmail.com>
2024-11-18 21:40:59 +0100

    start on test fixes

--- src/common/udp_server.rs
+++ src/common/udp_server.rs
@@ -1,4 +1,4 @@
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr};
 
 use super::{
     channel_socket::{ChannelPacket, ChannelSocket},
@@ -10,11 +10,13 @@ use tokio::{io::Result, net::UdpSocket, sync::mpsc::Sender};
 use tokio_util::sync::CancellationToken;
 use uuid::Uuid;
 
+type DataSender = Sender<Vec<u8>>;
+
 pub struct Client {
     pub id: Uuid,
     pub connection: Connection,
     pub address: SocketAddr,
-    pub socket_tx: Sender<Vec<u8>>,
+    pub socket_tx: DataSender,
     pub data: Vec<u8>,
 }
 
@@ -23,6 +25,7 @@ pub struct UdpServer {
     data_buffer: BytesMut,
     data_reciver_tx: Sender<ChannelPacket>,
     cancel_token: CancellationToken,
+    adress_to_tx_map: HashMap<SocketAddr, DataSender>,
 }
 
 impl UdpServer {
@@ -46,36 +49,56 @@ impl UdpServer {
             data_buffer,
             data_reciver_tx,
             cancel_token,
+            adress_to_tx_map: HashMap::new(),
         })
     }
 
     pub async fn listen_for_connections(&mut self) -> Result<Client> {
-        let (initial_data, address) = match self.socket.recv_from(&mut self.data_buffer).await {
-            Ok((size, address)) => (self.data_buffer[..size].to_vec(), address),
-            Err(e) => {
-                error!("Failed to read data from client: {}", e);
-                return Err(e);
+        loop {
+            let (data, address) = match self.socket.recv_from(&mut self.data_buffer).await {
+                Ok((size, address)) => (self.data_buffer[..size].to_vec(), address),
+                Err(e) => {
+                    error!("Failed to read data from client: {}", e);
+                    return Err(e);
+                }
+            };
+
+            if let Some(socket_tx) = self.adress_to_tx_map.get(&address) {
+                if !socket_tx.is_closed() {
+                    if let Err(e) = socket_tx.send(data).await {
+                        error!("Failed to send data to client: {}", e);
+                    }
+                    continue;
+                }
+
+                self.adress_to_tx_map.remove(&address);
             }
-        };
 
-        // TODO: Keep track of address, socket_tx. when existing client is found, send data to client via socket_tx. this will avoid overhead of creating new client for each packet.
+            let channel_socket = ChannelSocket::new(
+                self.data_reciver_tx.clone(),
+                self.cancel_token.child_token(),
+            );
 
-        let channel_socket = ChannelSocket::new(
-            self.data_reciver_tx.clone(),
-            self.cancel_token.child_token(),
-        );
+            let socket_tx = channel_socket.get_socket_tx();
 
-        let socket_tx = channel_socket.get_socket_tx();
-        Ok(Client {
-            id: channel_socket.get_id(),
-            connection: Connection::from(channel_socket),
-            address,
-            data: initial_data,
-            socket_tx,
-        })
+            self.adress_to_tx_map.insert(address, socket_tx.clone());
+
+            return Ok(Client {
+                id: channel_socket.get_id(),
+                connection: Connection::from(channel_socket),
+                address,
+                data,
+                socket_tx,
+            });
+        }
     }
 
     pub async fn write(&self, data: &[u8], address: &SocketAddr) -> Result<usize> {
         self.socket.send_to(data, address).await
+    }
+
+    pub async fn cleanup_inactive_clients(&mut self) {
+        // TODO: lets add this to every endpoint so that it can be called.
+        self.adress_to_tx_map.remove(address);
     }
 }
